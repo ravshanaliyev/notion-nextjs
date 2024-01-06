7039c20d7d5fa66e6052c4c51e6a69c03f99cd92
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-06 09:51:02 +0500

    mode toggle

--- app/(home)/components/navbar.tsx
+++ app/(home)/components/navbar.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { Logo } from './logo'
+import { ModeToggle } from '@/components/shared/mode-toggle'
 
 export const Navbar = () => {
     return (
-        <div className='z-50 bg-background fixed top-0 flex items-center w-full p-6'><Logo /></div>
+        <div className='z-50 bg-background fixed top-0 flex items-center w-full p-6 justify-between'>
+            <Logo />
+            <div className="flex items-center gap-x-2">
+                <ModeToggle />
+            </div>
+        </div>
     )
 }

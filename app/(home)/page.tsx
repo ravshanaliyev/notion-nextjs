187b09ba55b6a4e922f2e45f109ae0bc221c5159
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-06 10:06:36 +0500

    hero section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,11 @@
+import { Heroes } from "./components/heroes";
 
 export default function Home() {
   return (
-    <div className="text-3xl">
-      Hello
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heroes />
+      </div>
     </div>
-  )
+  );
 }

c31e146f4947c2ebb8aa8add7f700d766c6d9708
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-06 09:40:54 +0500

    images and half navbar

--- app/(home)/components/navbar.tsx
+++ app/(home)/components/navbar.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Logo } from './logo'
 
 export const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='z-50 bg-background fixed top-0 flex items-center w-full p-6'><Logo /></div>
     )
 }

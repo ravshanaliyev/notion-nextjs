7d96a32a4f8084411b01f8c07d3292beb128b130
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-06 09:54:16 +0500

    navbar

--- app/(home)/components/navbar.tsx
+++ app/(home)/components/navbar.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Logo } from './logo'
 import { ModeToggle } from '@/components/shared/mode-toggle'
+import { Button } from '@/components/ui/button'
 
 export const Navbar = () => {
     return (
         <div className='z-50 bg-background fixed top-0 flex items-center w-full p-6 justify-between'>
             <Logo />
             <div className="flex items-center gap-x-2">
+                <Button variant="ghost" size="sm">Log In</Button>
+                <Button size="sm">Get Notion Free</Button>
                 <ModeToggle />
             </div>
         </div>

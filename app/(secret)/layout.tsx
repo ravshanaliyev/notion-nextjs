ffe4b29525442a0bf3130dab94c3425e2726d5ac
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-07 11:47:10 +0500

    documents page for authenthicated users

---
+++ app/(secret)/layout.tsx
@@ -0,0 +1,10 @@
+import { ChildProps } from '@/types'
+import React from 'react'
+
+const SecretLayout = ({ children }: ChildProps) => {
+    return (
+        <div>{children}</div>
+    )
+}
+
+export default SecretLayout

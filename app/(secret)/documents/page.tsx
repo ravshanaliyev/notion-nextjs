6da0800e9eec9cf35bcac1fed89b08cef8e89d6c
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-07 11:51:14 +0500

    documents page

--- app/(secret)/documents/page.tsx
+++ app/(secret)/documents/page.tsx
@@ -1,35 +1,14 @@
 "use client";
 
-import { Loader } from "@/components/ui/loader";
-import { ChildProps } from "@/types";
-import { useConvexAuth } from "convex/react";
-import { redirect } from "next/navigation";
 import React from "react";
-import { Sidebar } from "../components";
 
-const SecretLayout = ({ children }: ChildProps) => {
-    const { isAuthenticated, isLoading } = useConvexAuth();
-
-    if (isLoading) {
-        return (
-            <div className="w-full h-full flex items-center justify-center">
-                <Loader size={"lg"} />
-            </div>
-        );
-    }
-
-    if (!isAuthenticated) {
-        return redirect("/");
-    }
-
+const DocumentPage = () => {
     return (
-        <div className="flex w-full">
-            <Sidebar />
-            <main className="flex-1 h-full overflow-y-auto">
-                {children}
-            </main>
+        <div className="h-screen w-full flex justify-center items-center space-y-4 flex-col">
+
+            <h1 className="text-3xl font-bold">Documents</h1>
         </div>
     );
 };
 
-export default SecretLayout;
+export default DocumentPage;
--- app/(secret)/layout.tsx
+++ app/(secret)/layout.tsx
@@ -1,10 +1,34 @@
-import { ChildProps } from '@/types'
-import React from 'react'
+
+import { Loader } from "@/components/ui/loader";
+import { ChildProps } from "@/types";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+import React from "react";
+import { Sidebar } from "./components";
 
 const SecretLayout = ({ children }: ChildProps) => {
-    return (
-        <div>{children}</div>
-    )
-}
+    const { isAuthenticated, isLoading } = useConvexAuth();
 
-export default SecretLayout
+    if (isLoading) {
+        return (
+            <div className="w-full h-full flex items-center justify-center">
+                <Loader size={"lg"} />
+            </div>
+        );
+    }
+
+    if (!isAuthenticated) {
+        return redirect("/");
+    }
+
+    return (
+        <div className="flex w-full">
+            <Sidebar />
+            <main className="flex-1 h-full overflow-y-auto">
+                {children}
+            </main>
+        </div>
+    );
+};
+
+export default SecretLayout;

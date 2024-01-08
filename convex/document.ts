0afa689b119308493afc1e8ed1edcbfd526aa916
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-08 18:33:36 +0500

    creating a document

--- convex/document.ts
+++ convex/document.ts
@@ -1,7 +1,7 @@
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 
-export const createTask = mutation({
+export const createDocument = mutation({
     args: {
         title: v.string(),
         parentDocument: v.optional(v.string()),
@@ -20,5 +20,27 @@ export const createTask = mutation({
             isPublished: false,
         });
         return document;
+    },
+});
+
+export const getDocuments = query({
+    args: {
+        parentDocument: v.optional(v.id("documents")),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+        const userId = identity.subject;
+        const documents = await ctx.db
+            .query("documents")
+            .withIndex("by_user_parent", (q) =>
+                q.eq("userId", userId).eq("parentDocument", args.parentDocument)
+            )
+            .filter((q) => q.eq(q.field("isArchived"), false))
+            .order("desc")
+            .collect();
+        return documents;
     },
 });
